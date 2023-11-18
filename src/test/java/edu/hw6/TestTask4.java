aea57b8efc01194eeb7b6af0985bfd6fcb8ed9c2
RuslanAndrianov <ruslandomoney@mail.ru>
2023-11-18 22:39:45 +0300

    100% готово (3) (исправлен тест)

--- src/test/java/edu/hw6/TestTask4.java
+++ src/test/java/edu/hw6/TestTask4.java
@@ -67,7 +67,6 @@ public class TestTask4 {
                 LOGGER.info("Не удалось создать файл!");
             }
         }
-        assertTrue(exists(path));
 
         streamComposition(path);
 
