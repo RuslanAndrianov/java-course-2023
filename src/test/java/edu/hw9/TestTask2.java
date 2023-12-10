84b2221152893ea72549c3c99a7bde6ecb1a2105
RuslanAndrianov <ruslandomoney@mail.ru>
2023-12-10 22:10:49 +0300

    Исправлены пути для теста

--- src/test/java/edu/hw9/TestTask2.java
+++ src/test/java/edu/hw9/TestTask2.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestTask2 {
 
-    String rootStr = System.getProperty("user.dir") + "/src/test/resources/testRootDir";
+    String rootStr = System.getProperty("user.dir") + "\\src\\test\\resources\\testRootDir";
 
 //    Структура тестовой папки:
 //
@@ -41,19 +41,19 @@ public class TestTask2 {
     void createTestDirectory() {
         try {
             createDirectory(Path.of(rootStr));
-            createDirectory(Path.of(rootStr + "/test1"));
-            createDirectory(Path.of(rootStr + "/test2"));
-            createDirectory(Path.of(rootStr + "/test3"));
-            createDirectory(Path.of(rootStr + "/test1/test11"));
-            createFile(Path.of(rootStr + "/test1/test11.txt"));
-            createFile(Path.of(rootStr + "/test1/test12.txt"));
-            createFile(Path.of(rootStr + "/test1/test11/test111.txt"));
-            createFile(Path.of(rootStr + "/test2/test21.md"));
-            createFile(Path.of(rootStr + "/test2/test22.txt"));
-            try (FileWriter writer11 = new FileWriter(rootStr + "/test1/test11.txt", true);
-                 FileWriter writer12 = new FileWriter(rootStr + "/test1/test12.txt", true);
-                 FileWriter writer111 = new FileWriter(rootStr + "/test1/test11/test111.txt", true);
-                 FileWriter writer21 = new FileWriter(rootStr + "/test2/test21.md", true)) {
+            createDirectory(Path.of(rootStr + "\\test1"));
+            createDirectory(Path.of(rootStr + "\\test2"));
+            createDirectory(Path.of(rootStr + "\\test3"));
+            createDirectory(Path.of(rootStr + "\\test1\\test11"));
+            createFile(Path.of(rootStr + "\\test1\\test11.txt"));
+            createFile(Path.of(rootStr + "\\test1\\test12.txt"));
+            createFile(Path.of(rootStr + "\\test1\\test11\\test111.txt"));
+            createFile(Path.of(rootStr + "\\test2\\test21.md"));
+            createFile(Path.of(rootStr + "\\test2\\test22.txt"));
+            try (FileWriter writer11 = new FileWriter(rootStr + "\\test1\\test11.txt", true);
+                 FileWriter writer12 = new FileWriter(rootStr + "\\test1\\test12.txt", true);
+                 FileWriter writer111 = new FileWriter(rootStr + "\\test1\\test11\\test111.txt", true);
+                 FileWriter writer21 = new FileWriter(rootStr + "\\test2\\test21.md", true)) {
                 writer11.write("abcde");
                 writer12.write("ab");
                 writer111.write("abc");
@@ -69,15 +69,15 @@ public class TestTask2 {
     @AfterEach
     void deleteTestDirectory() {
         try {
-            delete(Path.of(rootStr + "/test2/test22.txt"));
-            delete(Path.of(rootStr + "/test2/test21.md"));
-            delete(Path.of(rootStr + "/test1/test11/test111.txt"));
-            delete(Path.of(rootStr + "/test1/test12.txt"));
-            delete(Path.of(rootStr + "/test1/test11.txt"));
-            delete(Path.of(rootStr + "/test1/test11"));
-            delete(Path.of(rootStr + "/test3"));
-            delete(Path.of(rootStr + "/test2"));
-            delete(Path.of(rootStr + "/test1"));
+            delete(Path.of(rootStr + "\\test2\\test22.txt"));
+            delete(Path.of(rootStr + "\\test2\\test21.md"));
+            delete(Path.of(rootStr + "\\test1\\test11\\test111.txt"));
+            delete(Path.of(rootStr + "\\test1\\test12.txt"));
+            delete(Path.of(rootStr + "\\test1\\test11.txt"));
+            delete(Path.of(rootStr + "\\test1\\test11"));
+            delete(Path.of(rootStr + "\\test3"));
+            delete(Path.of(rootStr + "\\test2"));
+            delete(Path.of(rootStr + "\\test1"));
             delete(Path.of(rootStr));
         } catch (IOException e) {
             System.out.println("Ошибка при удалении тестовой директории!");
@@ -88,7 +88,7 @@ public class TestTask2 {
     @DisplayName("Тест DirectorySearch")
     void testDirectorySearch() {
         List<Path> directories1 = findDirectories(Path.of(rootStr), 2);
-        assertEquals(directories1, List.of(Path.of(rootStr + "/test1"), Path.of(rootStr)));
+        assertEquals(directories1, List.of(Path.of(rootStr + "\\test1"), Path.of(rootStr)));
 
         List<Path> directories2 = findDirectories(Path.of(rootStr), 100);
         assertEquals(directories2, List.of());
@@ -99,11 +99,11 @@ public class TestTask2 {
     void testFileSearch() {
         List<Path> files1 = findFiles(Path.of(rootStr), 3, 5, "txt");
         assertEquals(files1, List.of(
-            Path.of(rootStr + "/test1/test11.txt"),
-            Path.of(rootStr + "/test1/test11/test111.txt")));
+            Path.of(rootStr + "\\test1\\test11.txt"),
+            Path.of(rootStr + "\\test1\\test11\\test111.txt")));
 
         List<Path> files2 = findFiles(Path.of(rootStr), 3, 5, "md");
-        assertEquals(files2, List.of(Path.of(rootStr + "/test2/test21.md")));
+        assertEquals(files2, List.of(Path.of(rootStr + "\\test2\\test21.md")));
 
         List<Path> files3 = findFiles(Path.of(rootStr), 10, 20, "txt");
         assertEquals(files3, List.of());
